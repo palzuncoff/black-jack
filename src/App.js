b99f8ed60eda5241adb70df4224eb6c3898b9a81
Dmitrii Lazucov <palzuncoff@MacBook-Dmitrii.local>
2018-01-29 18:46:59 +0200

    Move game table to owne component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import React, { Component } from 'react';
-import nine_of_diamonds from './SVG-cards/9_of_diamonds.svg';
-import CardBack from './SVG-cards/Card_back_01.svg';
+import React, { Component } from 'react';;
 import './App.css';
+import Table from './components/Table';
 
 class App extends Component {
     render() {
         return (
-            <div className="field">
-                <h1>BLACK JACK</h1>
-                <h6> Dealer must draw to 16 and stand on all 17s</h6>
-                <h6>PAYS <span className="number">3</span> to <span className="number">2</span></h6>
-                <div className="flex-container">
-                    <div><img src={nine_of_diamonds} alt="" className="card" /></div>
-                    <div><img src={CardBack} alt="" className="card" /></div>
-                </div>
-                <div className="table-spliter" />
-                <div className="flex-container">
-                    <div><img src={nine_of_diamonds} alt="" className="card" /></div>
-                    <div><img src={nine_of_diamonds} alt="" className="card" /></div>
-                </div>
-            </div>
+            <Table/>
         );
     }
 }
